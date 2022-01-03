54ea3f56bed9bcf2fc6be19564d715d404541366
psw <tlsdnd001@naver.com>
2022-01-03 14:37:01 +0900

    django course 17 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,11 +3,21 @@ from django.shortcuts import render
 
 
 # Create your views here.
+from accountapp.models import HelloWorld
+
 
 def hello_world(request):
 
     if request.method == "POST":
+        
+        # hello_world_input 이라는 name의 값을 가져와 temp에 저장
+        temp = request.POST.get('hello_world_input')
+
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
         # context : 데이터 꾸러미
-        return render(request, 'accountapp/hello_world.html', context={'text': 'POST METHOD'})
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world})
     else:
         return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD'})
