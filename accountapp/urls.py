9dae1fe8411f83c0516a9c85ca546d56f4f82e80
psw <tlsdnd001@naver.com>
2022-01-03 22:08:48 +0900

    django course 21 commit

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from accountapp.views import hello_world
+from accountapp.views import hello_world, AccountCreateView
 
 app_name = "accountapp"
 
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
+    path('create/', AccountCreateView.as_view(), name='create'),    # Class 호출 시 .as_view() 사용, 함수 호출 시 함수명만 사용
 ]
