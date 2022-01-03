86f33db24d46f7dbdd6f4ee081549cc60778f121
psw <tlsdnd001@naver.com>
2022-01-03 23:54:54 +0900

    django course 24 commit

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accountapp.views import hello_world, AccountCreateView
+from accountapp.views import hello_world, AccountCreateView, AccountDetailView
 
 app_name = "accountapp"
 
@@ -12,4 +12,5 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
 
     path('create/', AccountCreateView.as_view(), name='create'),    # Class 호출 시 .as_view() 사용, 함수 호출 시 함수명만 사용
+    path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
 ]
